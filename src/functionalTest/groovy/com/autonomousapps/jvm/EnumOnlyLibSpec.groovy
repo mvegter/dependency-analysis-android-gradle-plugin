fad1ea688e196cc775c0855ff7fe9d5f631be63b
Martijn Vegter <martijn@martijnvegter.com>
2021-10-09 16:10:44 +0200

    Added reproduction test for incorrectly marked compileOnly dependency

---
+++ src/functionalTest/groovy/com/autonomousapps/jvm/EnumOnlyLibSpec.groovy
@@ -0,0 +1,26 @@
+package com.autonomousapps.jvm
+
+import com.autonomousapps.jvm.projects.EnumOnlyLibProject
+import spock.lang.Unroll
+
+import static com.autonomousapps.utils.Runner.build
+import static com.google.common.truth.Truth.assertThat
+
+final class EnumOnlyLibSpec extends AbstractJvmSpec {
+
+  @Unroll
+  def "detects enum-only libraries not marked as compileOnly (#gradleVersion)"() {
+    given:
+    def project = new EnumOnlyLibProject()
+    gradleProject = project.gradleProject
+
+    when:
+    build(gradleVersion, gradleProject.rootDir, ':buildHealth')
+
+    then:
+    assertThat(actualAdvice('proj')).containsExactlyElementsIn(project.expectedAdvice)
+
+    where:
+    gradleVersion << gradleVersions()
+  }
+}
